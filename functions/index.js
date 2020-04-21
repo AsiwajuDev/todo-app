d52215c67477849cbd7235a0bd0b0eb52f02d21d
asiwajudev <olanrewajuranda@gmail.com>
2020-04-22 00:06:41 +0100

    Express installed, started creating API function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const app = require("express")();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const { getAllTodos } = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+app.get("*/timestamp", (request, response) => {
+  response.send(`${Date.now()}`);
+});
+
+exports.api = functions.https.onRequest(app);

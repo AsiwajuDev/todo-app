c53cf4f6e2ead1dd102a65eb1c7113827501a340
asiwajudev <olanrewajuranda@gmail.com>
2020-04-22 01:53:58 +0100

    Firebase database created and connected

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,23 @@
+const { db } = require("../Utils/admin");
+
 exports.getAllTodos = (request, response) => {
-  todos = [
-    {
-      id: "1",
-      title: "greeting",
-      body: "Hello world from sharvin shah",
-    },
-    {
-      id: "2",
-      title: "greeting2",
-      body: "Hello2 world2 from sharvin shah",
-    },
-  ];
-  return response.json(todos);
+  db.collection("todos")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let todos = [];
+      data.forEach((doc) => {
+        todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return response.json(todos);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
 };
